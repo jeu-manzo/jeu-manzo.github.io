cee11b892ab8f4b014c7dbeba5b9eb00258413e8
Jeu Manzo <jeunettemanzo@gmail.com>
2019-06-21 22:13:58 -0500

    Updates

---
+++ precache-manifest.9f7efd9f16d84c598e7768d0181ac235.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e13a5260a9718480b4ec94c8da359b88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "328fbdd1e27ab8f13191",
+    "url": "/static/css/main.1cad76a2.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "328fbdd1e27ab8f13191",
+    "url": "/static/js/main.6146b9fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "145aaa8e0af68e8a15989e49aede4a96",
+    "url": "/static/media/background.145aaa8e.svg"
+  },
+  {
+    "revision": "8687e480a093815d8055e4ff17c279a5",
+    "url": "/static/media/logo-bco-jeu.8687e480.png"
+  }
+]);
