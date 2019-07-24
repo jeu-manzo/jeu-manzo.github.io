99303cd7b842d139b854b8253e9184c500a2bd8e
Jeu Manzo <jeunettemanzo@gmail.com>
2019-07-24 16:57:05 -0500

    create component AboutMe

---
+++ src/components/AboutMe.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class AboutMe extends Component {
+  render () {
+    return (
+      <section className="about-me">
+        <h1>Sobre Mí</h1>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+      </section>
+    )
+  }
+};
+
+export default AboutMe;
